db16551c882a37df4d31ff61065eb26f681bbb50
Morgunovv <morgunovv@users.noreply.github.com>
2025-11-27 10:09:53 +0100

    Добавлен код счетчика Yandex.Metrika на все страницы сайта

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,34 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        {/* Yandex.Metrika counter */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){
+                  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                  m[i].l=1*new Date();
+                  for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=105537008', 'ym');
+              ym(105537008, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+            `
+          }}
+        />
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/105537008" style={{position:'absolute', left:'-9999px'}} alt="" />
+          </div>
+        </noscript>
+        {/* /Yandex.Metrika counter */}
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
